3a638aa194ee2b83423d379c1c36e2eb7b3b478e
tjense25 <tanner.jensen08@gmail.com>
2018-03-12 23:27:11 -0600

    principal component analysis of species data

---
+++ Rscripts/PCA.R
@@ -0,0 +1,64 @@
+#!/apps/r/3.3.0/bin/Rscript
+
+#install necesary libraryies
+library(ggplot2)
+library(ggthemes)
+library(readr)
+library(dplyr)
+
+#Code to run Principal component analysis on data, view the multi dimensional
+#data in less dimensions
+
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args) == 0) {
+	stop("ERROR: Must specify input file as argument", call.=FALSE)
+}
+
+in_file <- args[1]
+fingerprints <- read_tsv(in_file)
+
+#remove the toxicity column of matrix and store result as matrix 
+characterMatrix <- as.matrix(select(fingerprints, -species))
+
+#get principal components from the matrix and store it in an object
+fingerprintPC <- prcomp(characterMatrix)
+
+#extract actual principal components and plot the first two PCs
+PCs <- as.data.frame(fingerprintPC$x)
+
+ggplot(PCs, aes(x=PC1, y=PC2, colour=fingerprints$species)) +
+	geom_point() +
+	labs(colour = "Species") + 
+	ggtitle("Principal Component Analysis Plot") +
+	theme_bw() + 
+	theme(text = element_text(size=20),
+	      plot.title = element_text(hjust = 0.5),
+	      panel.border = element_blank(),
+	      panel.grid.major = element_blank(),
+	      legend.position="none",
+	      axis.line = element_line(colour = "black"))
+
+
+ggsave("PCA.jpeg")
+
+#find out how much variance is explained by the first 10 principal components
+#and make a graphic to describe this
+
+percentVE <- 100 * fingerprintPC$sdev^2 / sum(fingerprintPC$sdev^2)
+
+percentVEdf <- data.frame(PC=1:10, PercentExplained=percentVE[1:10])
+
+ggplot(percentVEdf, aes(PC, PercentExplained, fill=PC)) +
+	geom_bar(stat="identity") +
+	xlab("Principal Component") +
+	ylab("% Variance explained") +
+	theme_bw() + 
+	theme(text = element_text(size=20),
+	      plot.title = element_text(hjust = 0.5),
+	      panel.border = element_blank(),
+	      panel.grid.major = element_blank(),
+	      legend.position="none",
+	      axis.line = element_line(colour = "black"))
+
+ggsave("varainceExplainedPCA.pdf")
--- Rscripts/runClassifier.R
+++ Rscripts/runClassifier.R
@@ -18,16 +18,17 @@ if (length(args) == 0) {
 in_file <- args[1]
 
 #Read data into a dataframe
-pepseq <- read_tsv(in_file)
+fingerprint <- read_tsv(in_file)
+
+str(fingerprint)
 
 #convert all characters into factors
-pepseq <- as.data.frame(unclass(pepseq))
+fingerprint <- as.data.frame(unclass(fingerprint))
 
-str(pepseq)
 
 #create a task object to indicate which data will be used,
 #and what is the target value for the classification
-task <- makeClassifTask(data = pepseq, target = "species")
+task <- makeClassifTask(data = fingerprint, target = "species")
 
 #Create a learner with the random Forest algorithm
 #specify we want the algorithm to generate probabilistic predictions
@@ -37,7 +38,7 @@ learner <- makeLearner("classif.randomForest", predict.type = "prob")
 resampleDesc = makeResampleDesc("CV", iters = 10)
 
 #set random seed, so it is computationally reproducible
-set.seed(129837192)
+set.seed(1)
 
 #make predictions by combinidng the data with the algorithm with the resampling strategy
 results <- resample(learner, task, resampleDesc, show.info=FALSE)
