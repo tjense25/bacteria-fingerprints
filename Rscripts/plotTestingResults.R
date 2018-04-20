63f26013ddddba8e486735b77d878a1487628824
tjense25 <tanner.jensen08@gmail.com>
2018-04-20 17:40:50 -0600

    side by side plot made

--- Rscripts/plotTestingResults.R
+++ Rscripts/plotTestingResults.R
@@ -4,6 +4,8 @@ library(ggplot2)
 library(ggthemes)
 
 speciesTestData <- read_tsv("output/speciesTestResult.out")
+mutationTestData <- read_tsv("output/randomMutationTest.out")
+print(mutationTestData)
 
 speciesPlot <- ggplot(speciesTestData, aes(numReads)) +
 		geom_line(aes(y=Accuracy, colour="Accuracy")) +
@@ -14,6 +16,18 @@ speciesPlot <- ggplot(speciesTestData, aes(numReads)) +
 				   labels = c(1,10,100,1000,10000,1000000)) +
 		scale_y_continuous("Model Performance", limits=c(0,1.0)) +
 		theme_bw() +
+		theme(text = element_text(size=16), legend.position="none")
+
+
+mutationPlot <- ggplot(mutationTestData, aes(mutRate)) +
+		geom_line(aes(y=Accuracy, colour="Accuracy")) +
+		geom_line(aes(y=AUC, colour="AUC")) +
+		labs(colour = "Performance Metric") + 
+		scale_x_continuous("Mutation Rate", limits=c(0,1.0)) +
+		scale_y_continuous("Model Performance", limits=c(0,1.0)) +
+		theme_bw() +
 		theme(text = element_text(size=16))
 
+require(gridExtra)
+combined <- grid.arrange(speciesPlot, mutationPlot, ncol=2)
 ggsave(speciesPlot, file="plots/speciesResultsPlot.jpeg")
