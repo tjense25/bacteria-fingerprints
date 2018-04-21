9870542fceb48ed5f4720c13dde2c9f580120eb4
tjense25 <tanner.jensen08@gmail.com>
2018-04-20 21:07:52 -0600

    deviation from normal plot

--- Rscripts/plotBaseSpaceCounts.R
+++ Rscripts/plotBaseSpaceCounts.R
@@ -18,6 +18,13 @@ in_file <- args[1]
 BPS <- read_delim(in_file, delim=" ", col_names=FALSE)
 names(BPS) <- c("name", "count", 1:286)
 
+BPS <- select(BPS, -count)
+BPS <- gather(BPS, "BPSIndex", "BPSFreq", 2:ncol(BPS))
+
+BPS$BPSIndex <- as.integer(BPS$BPSIndex)
+BPS$BPSFreq <- as.numeric(BPS$BPSFreq)
+BPS$name <- as.factor(BPS$name)
+
 bias <- NULL
 for (a in 0:10) {
 	for (c in 0:10) {
@@ -31,23 +38,36 @@ for (a in 0:10) {
 	}
 }
 
+normalizedBPS <- cbind(BPS)
+
+for (i in 1:nrow(BPS)) {
+	normalizedBPS[i,"BPSFreq"] = BPS[i,"BPSFreq"] - bias[ as.integer(BPS[i,"BPSIndex"])]
+}
+
+
 bias <- data.frame(BPSIndex = 1:286, BPSFreq = bias)
 
-BPS <- select(BPS, -count)
-BPS <- gather(BPS, "BPSIndex", "BPSFreq", 2:ncol(BPS))
-
-BPS$BPSIndex <- as.integer(BPS$BPSIndex)
-BPS$BPSFreq <- as.numeric(BPS$BPSFreq)
-BPS$name <- as.factor(BPS$name)
-
-print(BPS)
-plot <- ggplot(bias, aes(x=BPSIndex, y=BPSFreq)) +
+plot1 <- ggplot(bias, aes(x=BPSIndex, y=BPSFreq)) +
 	geom_line(alpha=0.5) +
 	geom_point(data=BPS, aes(colour=name)) + 
 	labs(colour="Species") + 
 	scale_x_continuous("Base Percentage Space 10-mers", limits=c(0,286)) +
-	scale_y_continuous("Probability of Sampling 10-mer") +
+	scale_y_continuous("10-mer Frequency") +
 	theme_bw() +
 	theme(text = element_text(size=16))
 
-ggsave(plot, file = "plots/streptococcusSpectrum.jpeg")
+ggsave(plot = plot1, file = "plots/streptococcusSpectrum.jpeg")
+
+plot2 <- ggplot(normalizedBPS, aes(x=BPSIndex, y=BPSFreq, colour=name)) +
+	 geom_line(alpha=0.5) +
+	 labs(colour="Species") + 
+	 scale_x_continuous("Base Percentage Space 10-mers", limits=c(0,286)) +
+	 scale_y_continuous("Deviation from Normal") +
+	 theme_bw() +
+	 theme(text = element_text(size=16))
+
+ggsave(plot = plot2, file = "plots/streptococcusDeviationFromNormal.jpeg")
+	 
+
+
+
